5ae109a21a725d03a6bbe61f6f3504837a9720e1
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-04-03 20:59:28 +0530

    Starting page added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser());
+app.use(morgan('combined'));
+
+app.set('view engine','pug');
 app.get('/', (req, res) => {
-  res.send('Hello World xDDS!')
+  res.render('index')
 })
 
 app.listen(port, () => {
