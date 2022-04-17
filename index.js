54b3c8a3b48707cb081a8ae4a8248198274c6e73
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-04-17 14:41:02 +0530

    Added knex.js

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
-//routes
+const db = require('./knex/knex');
+
 const voterRoutes = require('./routes/voters');
 const candidateRouter = require('./routes/candidates');
 const adminRouter = require('./routes/admin');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 app.use(cookieParser());
 app.use(morgan('combined'));
 app.use("/public", express.static(path.join(__dirname, 'public')));
 app.set('view engine','pug');
+
+// index route
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+// other routes
 app.use('/voters/',voterRoutes);
 app.use('/candidates/',candidateRouter);
 app.use('/admin/',adminRouter);
 
+
+// test database connection
+const testdb = async () => {
+  try 
+  {
+    const testKnexConnection = await db.raw("SELECT 1+1");
+  }
+  catch(err) 
+  {
+    console.log("Database Connection Error");
+    console.log(err);
+  }
+}
+
 app.listen(port, () => {
-  console.log(`Voting Website listening on port ${port}`)
+  testdb();
+  console.log(`Voting Website listening on port ${port}`);
 })
